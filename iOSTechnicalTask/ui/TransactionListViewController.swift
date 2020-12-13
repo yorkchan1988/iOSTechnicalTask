38d62a6600133ad359a27de0859c730eb6f83fc0
Chan, York T <york.t.chan@accenture.com>
2020-12-13 15:20:21 +0800

    create initial view controller by interface builder and start app programmatically

---
+++ iOSTechnicalTask/ui/TransactionListViewController.swift
@@ -0,0 +1,11 @@
+//
+//  TransactionListViewController.swift
+//  iOSTechnicalTask
+//
+//  Created by YORK CHAN on 13/12/2020.
+//
+
+import Foundation
+import UIKit
+
+class TransactionListViewController : UIViewController {}
---
+++ iOSTechnicalTask/ui/TransactionListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TransactionListViewModel.swift
+//  iOSTechnicalTask
+//
+//  Created by YORK CHAN on 13/12/2020.
+//
+
+import Foundation
