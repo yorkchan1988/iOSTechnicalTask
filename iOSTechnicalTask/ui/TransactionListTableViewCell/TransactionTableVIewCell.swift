a4eca5dc560efc08410dfa476c1ee850f04c6821
Chan, York T <york.t.chan@accenture.com>
2020-12-13 16:28:20 +0800

    complete transaction table view cell layout

---
+++ iOSTechnicalTask/ui/TransactionListTableViewCell/TransactionTableVIewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionListTableVIewCell.swift
+//  iOSTechnicalTask
+//
+//  Created by YORK CHAN on 13/12/2020.
+//
+
+import Foundation
+import UIKit
+
+class TransactionTableVIewCell: UITableViewCell {
+    
+    @IBOutlet weak var ivIcon: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblSubTitle: UILabel!
+    @IBOutlet weak var lblPrice: UILabel!
+}
