c2f84a6e6eb55636225d226056decac04090c601
Chan, York T <york.t.chan@accenture.com>
2020-12-14 22:38:44 +0800

    remove unused codes

--- iOSTechnicalTask/network/NetworkService.swift
+++ iOSTechnicalTask/network/NetworkService.swift
@@ -7,19 +7,13 @@
 
 import Foundation
 import RxAlamofire
-import Alamofire
 import RxSwift
 import ObjectMapper
-
-class Connectivity {
-    class var isConnectedToInternet:Bool {
-        return NetworkReachabilityManager()!.isReachable
-    }
-}
 
 class NetworkService {
     
     class func requestJSON<T : Mappable>(request: URLRequest, responseType: T.Type) -> Observable<T> {
+        
         return RxAlamofire.request(request)
             .validate(statusCode: 200..<300)
             .validate(contentType: ["application/json"])
